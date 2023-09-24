18e107e73a9f27b7246c8bd7de866cee7af2b35c
TarikMansour <121286624+TarikMansour@users.noreply.github.com>
2023-09-24 19:20:29 +0200

    Sistema_NumericoV2

--- Program.cs
+++ Program.cs
@@ -8,79 +8,58 @@ namespace Sistema_PosizionamentoNumerico
 {
     internal class Program
     {
-        static void Inserimento(int[] targa)
+        static int InserimentoLet()
         {
             //inserimento
-            Console.WriteLine("Inserire i 3 numeri della targa");
-            for (int i = 0; i < 3; i++)
-            {
-                targa[i] = Convert.ToInt32(Console.ReadLine());
-            }
             Console.WriteLine("Inserire le 4 lettere della targa nella stessa linea");
+            int vallettere=0;
             string lettere = Console.ReadLine().ToUpper(); //inserimento delle lettere come un array di carattere (stringa)
             //controllo: stringa deve essere avere 4 lettere in totale
-            if (lettere.Length < 4 || lettere.Length > 4){
+            if (lettere.Length < 4 || lettere.Length > 4)
+            {
                 Console.WriteLine("stringa invalida!");
             }
-            else{
+            else
+            {
                 for (int i = 0; i < lettere.Length; i++)
                 {
-                    char lettera = lettere[i]; //lettere della stringa vengono salvate dentro un char 
-                    int posizione = lettera - 'A' + 1; //calcolo della posizione della lettere sull'alfabeto sottraendo il valore ASCII di A dalla lettera inserita e + 1 per partire da 1 invece di 0
-                    targa[i + 3] = posizione; //partendo dall'indice 3 riempio l'array con le lettere
+
+                    char lettera = lettere[i];  
+                    int posizione = (lettera - 'A') * (int)Math.Pow(26, i);
+                    vallettere += posizione * (int)Math.Pow(10, 3);
+
                 }
             }
+            return vallettere;
         }
-        static void MoltiplicaECopia(int[] targa, long[] multinumeri, long[] multilettere) 
+        static int InserimentoNum()
         {
-            for (int i = 0; i < targa.Length; i++) 
+            Console.WriteLine("Inserire i 3 numeri della targa");
+            int valnumeri = 0;
+            for (int i = 0; i < 3; i++)
             {
-                int index = i;
-                targa[i] *= (int)Math.Pow(10, index); //funzione che moltiplica i numeri dentro l'array per 10 alla potenza del loro indice
-                //queste due metodi servono per copiare sia la parte numerica che letterale su due array di tipo long che hanno una capacita maggiore e anche per rendere piu facile i calcoli
-                //dall'array targa parto dall'indice 0, prendo 3 elementi (numeri) e gli sposto partendo dall'indice 0 a multinumeri
-                //dall'array targa parto dall'indice 3, prendo 4 elementi (lettere) e gli sposto partendo dall'indice 0 a multilettere
-                Array.Copy(targa, 0, multinumeri, 0, 3); 
-                Array.Copy(targa, 3, multilettere, 0, 4);
-            }
-            for (int i = 0; i < multilettere.Length; i++)
-            {
-                int index = i;
-                multilettere[i] *= (int)Math.Pow(26, index); //funzione che moltiplica i numeri dentro l'array di lettere per 26 alla potenza del loro indice
-            }
+                int numero = Convert.ToInt32(Console.ReadLine());
+                valnumeri = numero * (int)Math.Pow(10, i);
 
-
+            }
+            return valnumeri;
         }
+        static long Calcolo(int valnumeri, int vallettere)
+        {
+            long valore = valnumeri + vallettere;
+            return valore;
+        }
+
+    
         static void Main(string[] args)
         {
-            int[] targa = new int[7];
-            long[] multilettere = new long[4];
-            long[] multinumeri = new long[3];
-            long risultato = 0;
-            Inserimento(targa);
-            Console.WriteLine("Targa originale:");
-            for (int i = 0; i < targa.Length; i++)
-            {
-                Console.Write(targa[i] + " ");
-            }
-            Console.WriteLine();
-            MoltiplicaECopia(targa, multinumeri, multilettere);
-            Console.WriteLine("Targa completa:");
-            for (int i = 0; i < multinumeri.Length; i++)
-            {
-                Console.Write(multinumeri[i] + " ");
-            }
-            for (int i = 0; i < multilettere.Length; i++)
-            {
-                Console.Write(multilettere[i] + " ");
-            }
-            for (int i = 0; i < multilettere.Length; i++)
-            {
-                risultato += multilettere[i];
-            }
-            Console.WriteLine();
-            Console.WriteLine("Risultato:");
-            Console.WriteLine(risultato);
+            int valnumeri = InserimentoNum();
+            int vallettere = InserimentoLet();
+            long valore;
+          
+            valore = Calcolo(valnumeri,vallettere);
+            Console.WriteLine($"il risultato: {valore}");
+          
             Console.ReadLine();
         }
     }
